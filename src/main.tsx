1adb97410de65f448c84988110346295bd900fa1
Nishant Agnihotri <nishant.na.agnihotri@gmail.com>
2026-04-07 12:42:06 +0530

    fix: move body margin reset to dedicated reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { DebateScreen } from './components/DebateScreen';
+import './styles/reset.css';
 import './styles/tokens.css';
 
 const rootElement = document.getElementById('root');
